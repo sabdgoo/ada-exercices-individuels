08727a8cbff277fae325399eb1d38468dd4abcb2
sabdgoo <98774447+sabdgoo@users.noreply.github.com>
2022-02-09 21:58:43 +0100

    completed step 3

--- bonjour-javascript/html_template/js/script.js
+++ bonjour-javascript/html_template/js/script.js
@@ -1,16 +1,15 @@
-var student = prompt ("Please enter your name", "Your name") 
-var year = prompt ("Please enter your birth year", "YYYY")
-if (student != null) {
-var greeting = "Yo " + student + ", welcome to Ada Tech School!" + " You were born in " + year ;
-    
-    document.getElementById("Coucou").innerHTML = greeting ;    
-
+function askName() {
+    var student_name = prompt("Please enter your name", "Your name")
+    var greeting = "Yo " + student_name + ", welcome to Ada Tech School!";
+    document.getElementById("Coucou").innerHTML = greeting;
 }
- else {
- alert ("Please enter your name here")
+
+function askBirthYear() {
+    var year = prompt("Please enter your birth year", "YYYY")
+    var today = new Date().getFullYear()
+    var age = "You are " + (today - year) + " years old."
+    document.getElementById("Age").innerHTML = age; 
 }
-var today = new Date().getFullYear()
 
-var age = "You are " + (today - year) + " years old."
-
-document.getElementById("Age").innerHTML = age 
+askName()
+askBirthYear()
