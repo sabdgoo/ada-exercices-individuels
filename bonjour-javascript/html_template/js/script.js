20a019c2728feec88e2d9e4b1833bb129215ce8d
sabdgoo <98774447+sabdgoo@users.noreply.github.com>
2022-02-11 14:57:07 +0100

    compledted bonus exercise

--- bonjour-javascript/html_template/js/script.js
+++ bonjour-javascript/html_template/js/script.js
@@ -7,9 +7,24 @@ function askName() {
 function askBirthYear() {
     var year = prompt("Please enter your birth year", "YYYY")
     var today = new Date().getFullYear()
-    var age = "You are " + (today - year) + " years old."
-    document.getElementById("Age").innerHTML = age; 
+    var age = "You are gonna be " + (today - year) + " this year."
+    document.getElementById("Age").innerHTML = age;
+}
+
+function askBirthDay() {
+    var birth_day = prompt("Please enter your birth month", "YYYY-MM-DD")
+    var birthDate = new Date(birth_day)
+    var current_date = new Date()
+    var real_bd = current_date - birthDate;
+    var diffAge = (real_bd / (1000 * 60 * 60 * 24 * 365))
+    var accurateAge = Math.floor(diffAge)
+    var true_age = "For now, you are " + (accurateAge) + " years old."
+    document.getElementById("Real").innerHTML = true_age;
+    
 }
 
 askName()
 askBirthYear()
+askBirthDay()
+
+// what type does prompt trigger / how to convert string into date // how to sub two dates in JS 
